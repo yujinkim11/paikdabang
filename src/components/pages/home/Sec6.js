a1d7a5d560b1d2924ea558ea92edfe9189bc1496
yujinkim11 <wowyj26@naver.com>
2022-07-30 22:11:41 +0900

    sec6 완료

--- src/components/pages/home/Sec6.js
+++ src/components/pages/home/Sec6.js
@@ -1,16 +1,85 @@
 import styled from "styled-components";
+import { mainStyle } from "../../../styles/globalStyle";
 
 const Sec6Wrap = styled.div`
-  margin-top: 100px;
+  margin: 100px 0 200px 0;
 `;
-const SecBoxWrap = styled.div``;
-const Box = styled.div``;
+const SecBoxWrap = styled.div`
+  padding: ${mainStyle.padding};
+  display: flex;
+  justify-content: center;
+`;
+
+const Box = styled.div`
+  width: 620px;
+  height: 400px;
+  border: 7px solid ${mainStyle.maincolor};
+  border-radius: 28px;
+  transition: 1s;
+  cursor: pointer;
+  :nth-child(1) {
+    margin-right: 100px;
+  }
+`;
+
+const BlackBox = styled.div`
+  width: 613px;
+  height: 393px;
+  border-radius: 28px;
+  background-color: rgba(0, 0, 0, 0.2);
+  transition: 1s;
+  :hover {
+    background-color: rgba(0, 0, 0, 0.5);
+    .con {
+      color: #fee600;
+    }
+  }
+`;
+
+const BoxCon = styled.div`
+  color: white;
+  padding: 50px;
+  h3 {
+    font-size: 60px;
+    font-weight: 900;
+    margin-bottom: 20px;
+  }
+  p {
+    width: 250px;
+    font-size: 24px;
+    font-weight: 700;
+    line-height: 35px;
+  }
+`;
 
 export const Sec6 = () => {
   return (
     <Sec6Wrap>
       <SecBoxWrap>
-        <Box></Box>
+        <Box
+          style={{
+            background: `url(https://paikdabang.com/wp-content/themes/paikdabang/assets/images/main_sec5.jpg) no-repeat center / cover`,
+          }}
+        >
+          <BlackBox>
+            <BoxCon className="con">
+              <h3>매장 찾기</h3>
+              <p>가까운 빽다방 매장을 찾아보세요!</p>
+            </BoxCon>
+          </BlackBox>
+        </Box>
+        <Box
+          style={{
+            background: `url(https://paikdabang.com/wp-content/themes/paikdabang/assets/images/main_sec4.jpg) no-repeat center / cover`,
+          }}
+        >
+          <BlackBox>
+            <BoxCon className="con">
+              <h3>창업 문의</h3>
+              <p>쉽고 빠른 빽다방 창업 저희가 먼저 찾아갑니다!</p>
+            </BoxCon>
+          </BlackBox>
+        </Box>
       </SecBoxWrap>
     </Sec6Wrap>
   );
